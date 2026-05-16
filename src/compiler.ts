dd05bf46fe461666ee556fb3a38d899c65a9c72b
Layerscript <tevfik.fikret2026@gmail.com>
2026-05-16 17:05:15 +0300

    Create compiler.ts

---
+++ src/compiler.ts
@@ -0,0 +1,11 @@
+// src/compiler.ts
+import { ASTNode } from "./parser";
+
+export function compile(ast: ASTNode): string {
+    let outputJS = `// Compiled by LayerScript Compiler\n`;
+    
+    // İlerleyen aşamalarda AST düğümlerini gezerek (Tree Traversal)
+    // Reaktif DOM güncellemelerini ve stil yönetim kodlarını buraya üreteceğiz.
+    
+    return outputJS;
+}
